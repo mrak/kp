bdb2be37edf2698285367fa18f345420ab27381d
Eric Mrak <mail@ericmrak.info>
2017-09-29 16:36:18 -0700

    basic ui

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ extern crate clipboard;
 extern crate getopts;
 extern crate keepass;
 extern crate rpassword;
+extern crate cursive;
 
 use clipboard::{ClipboardContext,ClipboardProvider};
+use cursive::Cursive;
+use cursive::align::Align;
+use cursive::direction::Orientation;
+use cursive::views::{Dialog,BoxView,TextView,ListView,LinearLayout,Panel};
 use getopts::{Matches,Options};
 use keepass::{Database,Node,OpenDBError};
 use rpassword::prompt_password_stderr;
@@ -32,7 +37,41 @@ fn main() {
         .and_then(|mut db_handle| Database::open(&mut db_handle, &password))
         .unwrap();
 
-    dump_contents(db);
+    run(db_file, db);
+    //dump_contents(db);
+}
+
+fn run(file: String, db: Database) {
+    let mut ui = Cursive::new();
+    let entries_view = BoxView::with_full_screen(
+        Dialog::new()
+        .title("Entries")
+        .content(TextView::new("entries view").align(Align::center()))
+    );
+    let sidebar_view = BoxView::with_full_height(
+        Dialog::new()
+        .title("Groups")
+        .content(ListView::new())
+    );
+    let info_view = BoxView::with_full_width(
+        Dialog::new()
+        .title("Details")
+        .content(TextView::new("detail view").align(Align::center()))
+    );
+
+    ui.add_global_callback('q', |ui| ui.quit());
+    ui.add_fullscreen_layer(Dialog::new()
+        .title(format!("Database: {}", file))
+        .content(LinearLayout::new(Orientation::Horizontal)
+            .child(sidebar_view)
+            .child(LinearLayout::new(Orientation::Vertical)
+                .child(entries_view)
+                .child(info_view)
+            )
+        )
+    );
+
+    ui.run();
 }
 
 fn dump_contents(db: Database) {
